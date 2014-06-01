145ad0d817910ac99b26405ffff2c7cf6a84383f
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2014-06-01 16:08:24 -0400

    updated for changes in yml_reader

--- spec/fig_newton/fig_newton_spec.rb
+++ spec/fig_newton/fig_newton_spec.rb
@@ -5,7 +5,8 @@ describe FigNewton do
     it "should retrieve the data by a key named after the method called" do
       FigNewton.yml_directory = 'conf'
       yml_mock = double('yaml')
-      YAML.should_receive(:load_file).with('conf/test').and_return(yml_mock)
+      File.should_receive(:read).with('conf/test').and_return('test')
+      YAML.should_receive(:load).and_return(yml_mock)
       yml_mock.should_receive(:[]).with('desired_data').and_return('information')
       FigNewton.load('test')
       FigNewton.desired_data.should == 'information'
