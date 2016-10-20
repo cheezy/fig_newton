6a39409aa4056103bbe9eded97b22195d51918a4
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2016-10-19 19:36:47 -0500

    fixed broken spec

--- spec/fig_newton/fig_newton_spec.rb
+++ spec/fig_newton/fig_newton_spec.rb
@@ -4,10 +4,9 @@ describe FigNewton do
   context "when asking for data" do
     it "should retrieve the data by a key named after the method called" do
       FigNewton.yml_directory = 'conf'
-      yml_mock = double('yaml')
+      yml_mock = {'desired_data' => 'information'}
       expect(File).to receive(:read).with('conf/test').and_return('test')
       expect(YAML).to receive(:load).and_return(yml_mock)
-      expect(yml_mock).to receive(:[]).with('desired_data').and_return('information')
       FigNewton.load('test')
       expect(FigNewton.desired_data).to eql 'information'
     end
